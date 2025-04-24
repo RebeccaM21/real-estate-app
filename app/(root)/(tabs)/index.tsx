09b8a01fa2043a0c277d38117b34ff9b974fdddd
RebeccaM24 <rebeccampi@hollandandbarrett.com>
2025-04-24 01:30:59 +0100

    feat: initial set up and tabs created

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to the app</Text>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,67 @@
+import {
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
+
+export default function SignIn() {
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
+
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to login");
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Looking to get into real estate?
+          </Text>
+          <Text className="text-3xl text-center font-rubik-bold text-black-300">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300 ">Your Ideal Home</Text>
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-10"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Login with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
